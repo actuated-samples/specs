7fa3848920ef4c97a8913c4603065dd3aae728eb
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-05-16 11:52:26 +0100

    Use mirror
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ check-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo tee /etc/docker/daemon.json
+
+
+docker run alpine:3.17 uname
+
+
+sudo cat /etc/containerd/config.toml 
--- specs.sh
+++ specs.sh
@@ -1,9 +1,85 @@
 #!/bin/bash
 
-sudo tee /etc/docker/daemon.json
+df -h /
+lsblk
 
+echo "Machine ID: $(sudo cat /etc/machine-id)"
+echo "DBUS machine ID: $(sudo cat /var/lib/dbus/machine-id)"
 
-docker run alpine:3.17 uname
+sudo rm -rf /var/lib/dbus/machine-id
+sudo systemd-machine-id-setup
 
+echo "Machine ID: $(sudo cat /etc/machine-id)"
+echo "DBUS machine ID: $(sudo cat /var/lib/dbus/machine-id)"
 
-sudo cat /etc/containerd/config.toml 
+sudo dbus-uuidgen --ensure=/etc/machine-id
+
+echo "Machine ID: $(sudo cat /etc/machine-id)"
+echo "DBUS machine ID: $(sudo cat /var/lib/dbus/machine-id)"
+
+echo What was the start-time of the system image?
+
+sudo systemd-analyze critical-chain
+
+sudo systemd-analyze
+
+sudo systemd-analyze blame
+
+sudo systemctl list-jobs
+
+echo Where is this runner?
+
+curl -s http://ip-api.com/json|jq
+
+echo Hostname: $(hostname)
+
+echo Whoami: $(whoami)
+
+echo lsblk
+
+lsblk
+
+echo Rotational disks? $(cat /sys/block/vda/queue/rotational)
+
+echo Installing hdparm
+
+sudo apt update -qqqy && sudo apt install -qqqy hdparm
+
+echo Read speed
+
+sudo hdparm -t $(mount |grep "/ "|cut -d " " -f1)
+
+echo Write speed
+
+sync; dd if=/dev/zero of=./tempfile bs=1M count=1024; sync
+
+echo Information on main disk
+
+df -h /
+
+echo Memory info
+free -h
+
+echo Total CPUs:
+echo CPUs: $(nproc)
+
+echo CPU Model
+cat /proc/cpuinfo |grep "model name"
+
+echo Kernel and OS info
+uname -a
+
+cat /etc/os-release
+
+echo PATH defined as:
+echo $PATH
+
+echo Public IP:
+
+curl -s -L -S https://checkip.amazonaws.com
+
+#echo Checking speed
+# sudo pip install speedtest-cli
+# speedtest-cli
+
+docker run alpine:3.17.1 cat /etc/os-release
