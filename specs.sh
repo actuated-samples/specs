2076b33866095257cc84b751ea0d85278fa413f9
Alex Ellis <alexellis2@gmail.com>
2023-05-16 11:48:08 +0100

    Update specs.sh

--- specs.sh
+++ specs.sh
@@ -78,8 +78,8 @@ echo Public IP:
 
 curl -s -L -S https://checkip.amazonaws.com
 
-echo Checking speed
-
+#echo Checking speed
 # sudo pip install speedtest-cli
 # speedtest-cli
 
+docker run alpine:3.17.1 cat /etc/os-release
